54fe4f56c0ed550f614e319cbd70cbfceef55f9d
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-28 21:13:48 -0300

    cria todas as activities

---
+++ app/src/main/java/br/com/digitalhouse/desafiowebservice/activity/ComicsListActivity.kt
@@ -0,0 +1,12 @@
+package br.com.digitalhouse.desafiowebservice.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.digitalhouse.desafiowebservice.R
+
+class ComicsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comics_list)
+    }
+}
--- app/src/main/java/br/com/digitalhouse/desafiowebservice/activity/SplashActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafiowebservice/activity/SplashActivity.kt
@@ -1,12 +1,24 @@
 package br.com.digitalhouse.desafiowebservice.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import br.com.digitalhouse.desafiowebservice.R
 
 class SplashActivity : AppCompatActivity() {
+
+    private val splashScreenTime : Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, splashScreenTime)
+
     }
 }
