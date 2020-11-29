38fbf5f618481c2fd172afe04d6be4c6da3c7239
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-28 22:45:56 -0300

    format login screen

--- app/src/main/java/br/com/digitalhouse/desafiowebservice/activity/LoginActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafiowebservice/activity/LoginActivity.kt
@@ -1,12 +1,26 @@
 package br.com.digitalhouse.desafiowebservice.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.digitalhouse.desafiowebservice.R
+import br.com.digitalhouse.desafiowebservice.helper.AsteriskPasswordTransformationMethod
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        inputPassword.transformationMethod =
+            AsteriskPasswordTransformationMethod()
+
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this, ComicsListActivity::class.java))
+        }
+
+        btnCadastro.setOnClickListener {
+            startActivity(Intent(this, ComicsListActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/br/com/digitalhouse/desafiowebservice/helper/AsteriskPasswordTransformationMethod.kt
@@ -0,0 +1,25 @@
+package br.com.digitalhouse.desafiowebservice.helper
+
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+
+class AsteriskPasswordTransformationMethod : PasswordTransformationMethod() {
+    override fun getTransformation(source: CharSequence?, view: View?): CharSequence {
+        return PasswordCharSequence(source!!)
+    }
+
+    private inner class PasswordCharSequence(private val mSource: CharSequence) : CharSequence {
+
+        override val length: Int
+            get() = mSource.length
+
+        override fun get(index: Int): Char {
+            return '*'
+        }
+
+        override fun subSequence(start: Int, end: Int): CharSequence {
+            return mSource.subSequence(start, end) // Return default
+        }
+
+    }
+}
